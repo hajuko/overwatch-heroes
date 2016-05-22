796991cf810afce0e17446aa414bde1a6173f345
Julien Thoma <julien.thoma89@gmail.com>
2016-05-22 16:07:24 +0200

    update

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var port = 8080;
 
 app.use(express.static(__dirname + '/public'));
 
@@ -8,7 +7,7 @@ app.get('/data', function(req, res) {
 
 });
 
-app.listen(port);
+app.listen(process.env.PORT  || 5000);
 console.log('started at port: ' + port);
 
 module.exports = app;
