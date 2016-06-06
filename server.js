f5009e068e11d73bba69fcff206e3819ebd0c6cb
Anton Stepan <info@antonstepan.it>
2016-06-06 10:55:06 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 6000;
 const cors = require('cors');
 
-app.use(express.static(__dirname + '/public'));
 app.disable('x-powered-by');
 app.use(cors());
 app.options('*', cors());
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT  || port);
 console.log('started at port: ' + port);
