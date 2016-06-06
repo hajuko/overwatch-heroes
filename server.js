7d322dab680572cf901ac3eb0b467123de831b95
Anton Stepan <info@antonstepan.it>
2016-06-06 10:47:43 +0200

    added cors to server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-var express = require('express');
-var app = express();
-var port = 6000;
+const express = require('express');
+const app = express();
+const port = 6000;
+const cors = require('cors');
 
 app.use(express.static(__dirname + '/public'));
+app.use(cors());
+app.options('*', cors());
 
 app.listen(process.env.PORT  || port);
 console.log('started at port: ' + port);
